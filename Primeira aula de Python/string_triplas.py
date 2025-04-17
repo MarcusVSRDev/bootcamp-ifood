4f5d97d668a8e07ebe52d10cc7bcc5473c8b0610
Marcus Romero <marcusvinicius616@gmail.com>
2025-04-17 15:11:17 -0300

    Delete Primeira aula de Python directory

--- Primeira aula de Python/string_triplas.py
+++
@@ -1,8 +0,0 @@
-#A variável f permite quebrar a string em várias linhas e também permite interpolação de variáveis dentro da string.
-nome = "Marcus"
-
-mensagem = f"""
-Olá, {nome}!
-Seja bem-vindo(a) à primeira aula de Python.
-"""
-print(mensagem)
